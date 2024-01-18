c5de24f9ae5dff6479cb4948c0c8f4bd3362f86e
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-01-18 16:59:18 +0100

    add: migration, model and controller for comics

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\ComicController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::resource('comics', ComicController::class);
+
