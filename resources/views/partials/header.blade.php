867ad31d9839442bce5d3e23813eca71fcf52c1a
andrearicciotti <ricciotti.andrea1993@gmail.com>
2024-01-19 10:41:40 +0100

    add: HomeController to manage home page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,7 @@
         <div class="container">
             <div class="row justify-content-between">
                 <div class="col-1 fs-1 fw-bold"><a href="{{ route('home') }}">Home</a></div>
+                <div class="col-1 fs-1 fw-bold"><a href="{{ route('comics.index') }}">Comics</a></div>
                 <div class="col-1 fs-1 fw-bold"><a href="{{ route('comics.create') }}">New</a></div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\ComicController;
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [ComicController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('comics', ComicController::class);
 
